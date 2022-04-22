8844563446681bd0223e51e47aae342919651d75
홍준성 <powerstar13@hanmail.net>
2022-04-22 18:06:29 +0900

    git init > MSA 설계 과제 > README 작성, YAML 설정, Eureka Client 역할 선언

---
+++ src/test/java/study/msa/msadesigntaskcommunity/MsaDesignTaskCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package study.msa.msadesigntaskcommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaDesignTaskCommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
